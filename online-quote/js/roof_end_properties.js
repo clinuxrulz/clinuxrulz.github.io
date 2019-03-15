0ad194b5ccaa9ac095ff77b7db16a53b0ac6674b
Clinton Selke <clinuxrulz@gmail.com>
2019-03-15 15:47:42 +1000

    updated online quote

---
+++ online-quote/js/roof_end_properties.js
@@ -0,0 +1,111 @@
+var initRoofEndProperties = (function() {
+    var nextId = 1;
+    var genId = function() {
+        return "roofEndPropertiesId" + (nextId++);
+    };
+    var findElementByClassNameOfParent = function(parent, className) {
+        var atNode, i, node;
+        var stack = [];
+        stack.push(parent);
+        while (stack.length != 0) {
+            atNode = stack.pop();
+            for (i = 0; i < atNode.childNodes.length; ++i) {
+                node = atNode.childNodes[i];
+                if (node.className == className) {
+                    return node;
+                }
+                stack.push(node);
+            }
+        }
+        return undefined;
+    };
+    return function(div, callbacks) {
+        var radRoofEndTypeGable = findElementByClassNameOfParent(div, "radRoofEndTypeGable");
+        var roofEndTypeGableId = genId();
+        radRoofEndTypeGable.id = roofEndTypeGableId;
+        findElementByClassNameOfParent(div, "lblRoofEndTypeGable").setAttribute("for", roofEndTypeGableId);
+        var radRoofEndTypeHip = findElementByClassNameOfParent(div, "radRoofEndTypeHip");
+        var roofEndTypeHipId = genId();
+        radRoofEndTypeHip.id = roofEndTypeHipId;
+        findElementByClassNameOfParent(div, "lblRoofEndTypeHip").setAttribute("for", roofEndTypeHipId);
+        var radRoofEndTypeDutchGable = findElementByClassNameOfParent(div, "radRoofEndTypeDutchGable");
+        var roofEndTypeDutchGableId = genId();
+        radRoofEndTypeDutchGable.id = roofEndTypeDutchGableId;
+        findElementByClassNameOfParent(div, "lblRoofEndTypeDutchGable").setAttribute("for", roofEndTypeDutchGableId);
+        var divDutchGableDistIn = findElementByClassNameOfParent(div, "divDutchGableDistIn");
+        var txtDutchGableDistIn = findElementByClassNameOfParent(div, "txtDutchGableDistIn");
+        var dutchGableDistInSlider = findElementByClassNameOfParent(div, "dutchGableDistInSlider");
+        $(dutchGableDistInSlider).slider({
+            "min": 100,
+            "max": 10000,
+            "step": 100,
+            "value": 1500,
+            "slide": function(event, ui) {
+                txtDutchGableDistIn.value = ui.value;
+                callbacks.setRoofEndDutchGableDistIn(ui.value);
+            }
+        });
+        txtDutchGableDistIn.value = "1500";
+        txtDutchGableDistIn.addEventListener("input", function() {
+            var value = parseFloat(txtDutchGableDistIn.value);
+            if (!isNaN(value)) {
+                $(dutchGableDistInSlider).slider({ "value": value });
+                callbacks.setRoofEndDutchGableDistIn(value);
+            }
+        });
+        radRoofEndTypeGable.addEventListener("change", function() {
+            if (radRoofEndTypeGable.checked) {
+                divDutchGableDistIn.style.display = "none";
+                callbacks.setRoofEndToGable();
+            }
+        });
+        radRoofEndTypeHip.addEventListener("change", function() {
+            if (radRoofEndTypeHip.checked) {
+                divDutchGableDistIn.style.display = "none";
+                callbacks.setRoofEndToHip();
+            }
+        });
+        radRoofEndTypeDutchGable.addEventListener("change", function() {
+            if (radRoofEndTypeDutchGable.checked) {
+                divDutchGableDistIn.style.display = "block";
+                callbacks.setRoofEndToDutchGable();
+            }
+        });
+    };
+})();
+
+initRoofEndProperties(
+    document.getElementById("divEnd1RoofEndProperties"),
+    {
+        setRoofEndToGable: function() {
+            app.setEnd1RoofEndToGable();
+        },
+        setRoofEndToHip: function() {
+            app.setEnd1RoofEndToHip();
+        },
+        setRoofEndToDutchGable: function() {
+            app.setEnd1RoofEndToDutchGable();
+        },
+        setRoofEndDutchGableDistIn: function(distIn) {
+            app.setEnd1RoofEndDutchGableDistIn(distIn);
+        }
+    }
+);
+
+initRoofEndProperties(
+    document.getElementById("divEnd2RoofEndProperties"),
+    {
+        setRoofEndToGable: function() {
+            app.setEnd2RoofEndToGable();
+        },
+        setRoofEndToHip: function() {
+            app.setEnd2RoofEndToHip();
+        },
+        setRoofEndToDutchGable: function() {
+            app.setEnd2RoofEndToDutchGable();
+        },
+        setRoofEndDutchGableDistIn: function(distIn) {
+            app.setEnd2RoofEndDutchGableDistIn(distIn);
+        }
+    }
+);
